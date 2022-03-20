7c34cbdd9c81788285aa87efd1ddaff76e68c17c
Francisco Galarza <franciscogalarza@gmail.com>
2022-03-20 12:24:46 +1100

    Stopped bootstrap from watching itself

--- cluster/argocd.sh
+++ cluster/argocd.sh
@@ -19,15 +19,15 @@ brew install argocd
 kubectl -n argocd rollout status deployment argocd-server
 kubectl port-forward svc/argocd-server -n argocd $PORT:443 &
 PORT_FORWARD_PID=$!
+echo "ARGOCD FORWARD PORT PID: $PORT_FORWARD_PID"
 
 # Update admin password
 export PASS=$(kubectl -n argocd get secret argocd-initial-admin-secret --output=jsonpath="{.data.password}" | base64 --decode)
 argocd login --insecure --username admin --password $PASS --grpc-web localhost:$PORT
 argocd account update-password --current-password $PASS --new-password $PASSWORD
 
-# Stop port forwarding
-# kill $PORT_FORWARD_PID
-
 # Initialise projects and apps
 kubectl apply -f argocd-apps
 
+# Stop port forwarding
+kill $PORT_FORWARD_PID
