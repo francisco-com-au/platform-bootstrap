032ef811547092d22465b01a0909240e49b71fe4
Francisco Galarza <franciscogalarza@gmail.com>
2022-06-19 16:48:50 +1000

    Fixed quotes

--- cluster/mount_secrets.sh
+++ cluster/mount_secrets.sh
@@ -6,5 +6,5 @@ PROJECT_ID=$(cat $GCP_KEY_PATH | jq -r '.project_id')
 # Crossplane GCP key
 kubectl create namespace crossplane-system
 kubectl -n crossplane-system create secret generic gcp-provider \
-    --from-literal=credentials.json=$(cat $GCP_KEY_PATH) \
+    --from-literal=credentials.json="$(cat $GCP_KEY_PATH)" \
     --from-literal=project_id=$PROJECT_ID
