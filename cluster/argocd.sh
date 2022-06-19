764b8fb351784747c7101b9d29c5cceab4dd31c0
Francisco Galarza <franciscogalarza@gmail.com>
2022-06-19 12:28:01 +1000

    Updated install script to use dns

--- cluster/argocd.sh
+++ cluster/argocd.sh
@@ -19,18 +19,26 @@ brew install argocd
 
 # Wait for the Server to be up
 kubectl -n argocd rollout status deployment argocd-server
-kubectl port-forward svc/argocd-server -n argocd $PORT:443 &
-PORT_FORWARD_PID=$!
-echo "ARGOCD FORWARD PORT PID: $PORT_FORWARD_PID"
-
-# Update admin password
-export PASS=$(kubectl -n argocd get secret argocd-initial-admin-secret --output=jsonpath="{.data.password}" | base64 --decode)
-argocd login --insecure --plaintext --username admin --password $PASS localhost:$PORT
-# argocd login --insecure --plaintext --username admin --password $PASS --grpc-web localhost:$PORT
-argocd account update-password --current-password $PASS --new-password $PASSWORD
 
 # Initialise projects and apps
 kubectl apply -k ./apps/bootstrap/overlays/$ENVIRONMENT
 
+# Wait for nginx to be up
+until kubectl -n ingress-nginx rollout status deployment ingress-nginx-controller; do
+    sleep 5
+done
+
+# Update admin password (need to configure argocd.this in your host file to point to 127.0.0.1)
+export PASS=$(kubectl -n argocd get secret argocd-initial-admin-secret --output=jsonpath="{.data.password}" | base64 --decode)
+argocd login argocd.this --insecure --username admin --password $PASS --grpc-web
+argocd account update-password --current-password $PASS --new-password $PASSWORD
+
+# Update password using port-forward (does not work)
+# kubectl port-forward svc/argocd-server -n argocd $PORT:443 &
+# PORT_FORWARD_PID=$!
+# echo "ARGOCD FORWARD PORT PID: $PORT_FORWARD_PID"
 # Stop port forwarding
-kill $PORT_FORWARD_PID
+# kill $PORT_FORWARD_PID
+# argocd login --insecure --plaintext --username admin --password $PASS --grpc-web localhost:$PORT
+
+
